9aab93fbefc740e28f39ee6a2c9b3efc55174ff2
DejanDjosic <ddjosic@gmail.com>
2024-01-11 23:27:11 +0100

    initial commit

---
+++ resources/views/terms.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.layout')
+@section('content')
+<h1>Terms</h1>
+<div>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas omnis eligendi quo perspiciatis fuga quam, autem est
+    iusto itaque. Tenetur ex accusamus neque doloremque pariatur sit delectus, quam temporibus distinctio?</div>
+@endsection
