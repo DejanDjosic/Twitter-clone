4e6d9465ecda7442dc715202d6ee11988c7b6260
DejanDjosic <ddjosic@gmail.com>
2024-01-21 15:31:51 +0100

    Added updating posts

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -10,12 +10,45 @@ class IdeaController extends Controller
     public function store()
     {
         request()->validate([
-            'idea'=>'required|min:5|max:240'
+            'content' => 'required|min:5|max:240'
         ]);
 
-        $idea = Idea::create(['content' => request()->get('idea', '')]);
+        $idea = Idea::create(['content' => request()->get('content', '')]);
 
         $idea->save();
         return redirect()->route('dashboard')->with('success', 'Idea was created successfully!');
+    }
+
+    public function show(Idea $idea)
+    {
+        return view(
+            'ideas.show',
+            ['idea' => $idea]
+        );
+    }
+    public function edit(Idea $idea)
+    {
+        $editing = true;
+        return view(
+            'ideas.show',
+            compact('idea', 'editing')
+
+        );
+    }
+
+    public function update(Idea $idea)
+    {
+        request()->validate([
+            'content' => 'required|min:5|max:240'
+        ]);
+        $idea->content=request()->get('content','');
+        $idea->save();
+        return redirect()->route('ideas.show',$idea->id)->with('success',"Idea was updated succesful");
+    }
+
+    public function destroy(Idea $idea)
+    {
+        $idea->delete();
+        return redirect()->route('dashboard')->with('success', 'Idea was deleted successfully!');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
 
-Route::post('/idea',[IdeaController::class,'store'])->name('ideas.store');
+Route::post('/ideas',[IdeaController::class,'store'])->name('ideas.store');
 
-Route::delete('/idea/{id}',[IdeaController::class,'store'])->name('ideas.destroy');
+Route::get('/ideas/{idea}',[IdeaController::class,'show'])->name('ideas.show');
+Route::get('/ideas/{idea}/edit',[IdeaController::class,'edit'])->name('ideas.edit');
+Route::put('/ideas/{idea}',[IdeaController::class,'update'])->name('ideas.update');
+
+
+
+Route::delete('/ideas/{idea}',[IdeaController::class,'destroy'])->name('ideas.destroy');
 
 
 Route::get('/profile', [ProfileController::class, 'index']);
